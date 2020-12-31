374b8cefa15dcb90ccf118b7e8d553f1059144d8
Mingyuan Hua <huamy2013@gmail.com>
2020-12-30 21:09:26 -0500

    finish part2 phonebook axios part

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [check, setCheck] = useState("");
   const [names, setNames] = useState(
     new Set(persons.map((person) => person.name))
   );
+
+  useEffect(() => {
+    // effect
+    axios.get("http://localhost:3001/persons").then((Response) => {
+      setPersons(Response.data);
+    });
+  }, []);
 
   const addPerson = (e) => {
     e.preventDefault();
