83b00d24cc798b40be1174dca5a06378205d94d0
Mingyuan Hua <huamy2013@gmail.com>
2020-12-30 20:05:44 -0500

    finish part2 phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,105 @@
 import React, { useState } from "react";
 
 const App = () => {
-  // eslint-disable-next-line
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  // eslint-disable-next-line
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [check, setCheck] = useState("");
+  const [names, setNames] = useState(
+    new Set(persons.map((person) => person.name))
+  );
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    };
+    if (names.has(newName)) {
+      window.alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat(personObject));
+      setNames(names.add(newName));
+      setNewName("");
+      setNewNumber("");
+    }
+  };
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
+
+  const handleCheckChange = (e) => {
+    setCheck(e.target.value);
+  };
+
+  const personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(check.toLowerCase())
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter check={check} handleCheckChange={handleCheckChange} />
+      <h2>add a new</h2>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
       <h2>Numbers</h2>
-      ...
+      <Persons persons={personsToShow} />
     </div>
+  );
+};
+
+const Filter = (props) => {
+  return (
+    <form>
+      filter shown with:
+      <input value={props.check} onChange={props.handleCheckChange} />
+    </form>
+  );
+};
+
+const PersonForm = (props) => {
+  return (
+    <form onSubmit={props.addPerson}>
+      <div>
+        name: <input value={props.newName} onChange={props.handleNameChange} />
+      </div>
+      <div>
+        number:
+        <input value={props.newNumber} onChange={props.handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ persons }) => {
+  let i = 0;
+  return (
+    <ul>
+      {persons.map((person) => (
+        <li key={++i}>
+          {person.name} {person.number}
+        </li>
+      ))}
+    </ul>
   );
 };
 
