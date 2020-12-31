e16cfd4a774994dc60df7c39365ea53dd38fabf9
Mingyuan Hua <huamy2013@gmail.com>
2020-12-31 01:08:18 -0500

    finish part2 countries

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,23 +1,42 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Countries from "./components/Countries";
 
 function App() {
+  // eslint-disable-next-line
+  const [countries, setCountries] = useState([]);
+  const [input, setInput] = useState("");
+  const [selected, setSelected] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then((Response) => setCountries(Response.data));
+  }, []);
+
+  const countriesToShow =
+    selected.length === 0
+      ? countries.filter((country) =>
+          country.name.toLowerCase().includes(input.toLowerCase())
+        )
+      : selected;
+
+  const handleSelect = ({ country }) => {
+    setSelected([country]);
+  };
+
+  const handleInput = (e) => {
+    if (selected.length === 1) {
+      setSelected([]);
+    }
+    return setInput(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Find countries:
+      <input value={input} onChange={handleInput} />
+      <Countries countries={countriesToShow} handleSelect={handleSelect} />
     </div>
   );
 }
